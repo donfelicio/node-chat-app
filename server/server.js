817ec3e63325737cbc590489b14e68a2790b0059
Felix Lepoutre <felix@mrflow.co>
2018-08-23 10:44:49 +0200

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -13,12 +13,24 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('new user connected');
-  // 
+  //
   // socket.emit('newMessage', {
   //   from: 'Jim',
   //   message: 'This is a chat message',
   //   createdAt: 123
   // });
+
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to this chat',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
@@ -27,6 +39,12 @@ io.on('connection', (socket) => {
       text: message.text,
       createdAt: new Date().getTime()
     });
+
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
