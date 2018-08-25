c9487a779b8ce390dc5b1524858e0eda28f88461
Felix Lepoutre <felix@mrflow.co>
2018-08-25 14:42:04 +0200

    added moment timestamps to chat

---
+++ playground/time.js
@@ -0,0 +1,8 @@
+// // const moment = require('moment');
+// //
+// // var createdAt = 1234233333234;
+// // var date = moment(createdAt);
+// //
+// // console.log(date.format('MMM Do YYYY'));
+//
+// var someTimeStamp = moment().valueOf();
